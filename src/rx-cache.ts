9a191c30e0339946c98b716902d53bb27ffcd568
hannahhoward <hannah@hannahhoward.net>
2018-05-27 14:10:44 -0700

    refactor(RxCache): Rewrite for clarity
    Make overall setup much clearer, clearly define cachestate

--- src/rx-cache.ts
+++ src/rx-cache.ts
@@ -1,128 +1,120 @@
-import {
-  Observable,
-  Subject,
-  ReplaySubject,
-  Timestamp,
-  combineLatest,
-  ConnectableObservable
-} from 'rxjs'
+import { Observable, Subject, pipe, OperatorFunction, merge, of } from 'rxjs'
 import {
   map,
   shareReplay,
   switchMap,
   timestamp,
-  startWith,
   take,
   flatMap,
   skip,
-  merge,
-  publish
+  scan,
+  startWith
 } from 'rxjs/operators'
 
 const CACHE_SIZE = 1
-
+const INITIAL_TIME = -1
 enum CacheEventType {
   Initial,
-  Update,
+  Response,
+  Request,
   Reset
 }
 
-interface CacheEventKind<T extends CacheEventType> {
-  type: T
-  timestamp: T extends CacheEventType.Update ? number : undefined
+interface CacheEvent {
+  type: CacheEventType
+  timestamp: number
 }
 
-type CacheEvent =
-  | CacheEventKind<CacheEventType.Initial>
-  | CacheEventKind<CacheEventType.Reset>
-  | CacheEventKind<CacheEventType.Update>
+interface CacheState {
+  lastEvent: CacheEventType
+  lastEventTimestamp: number
+  lastRequestTimestamp: number
+  lastResponseTimestamp: number
+}
+
+const toCacheEvent = (type: CacheEventType): OperatorFunction<any, CacheEvent> =>
+  pipe(timestamp(), map(({ timestamp }) => ({ type, timestamp })))
 
 function shouldMakeRequest(
-  lastRequestEvent: CacheEvent,
-  lastResponseEvent: CacheEvent,
+  cacheState: CacheState,
   currentTime: number,
-  expiry: number
+  ifNotOlderThan: number
 ): boolean {
-  switch (lastRequestEvent.type) {
+  switch (cacheState.lastEvent) {
     case CacheEventType.Initial:
     case CacheEventType.Reset:
       return true
     default:
-      let requestInExpiry = lastRequestEvent.timestamp + expiry < currentTime
-      switch (lastResponseEvent.type) {
-        case CacheEventType.Initial:
-        case CacheEventType.Reset:
-          return requestInExpiry
-        default:
-          return requestInExpiry && lastResponseEvent.timestamp + expiry < currentTime
-      }
+      return (
+        cacheState.lastRequestTimestamp + ifNotOlderThan < currentTime &&
+        cacheState.lastResponseTimestamp + ifNotOlderThan < currentTime
+      )
   }
 }
 
 export default class RxCache<T> {
-  private requests: Subject<void> = new Subject()
-  private requestEvents: Observable<CacheEvent>
-  private responses: Observable<T>
-  private responseEvents: Observable<CacheEvent>
-  private responseReceived: Observable<boolean>
-  private resetCache: Subject<void> = new Subject()
+  private requests$: Subject<void> = new Subject()
+  private cacheState$: Observable<CacheState>
+  private responses$: Observable<T>
+  private resetCacheRequests$: Subject<void> = new Subject()
   constructor(protected observableOperation$: Observable<T>) {
-    // FIXME: explicit typecast is due to
-    // https://github.com/ReactiveX/rxjs/issues/2972
-    // correct when fixed
-    let resetEvents = this.resetCache.pipe(
-      map<void, CacheEvent>(_ => ({ type: CacheEventType.Reset, timestamp: undefined })),
-      publish()
-    ) as ConnectableObservable<CacheEvent>
-    resetEvents.connect()
-
-    this.requestEvents = this.requests.pipe(
-      timestamp(),
-      map<Timestamp<void>, CacheEvent>(timestamp => ({
-        type: CacheEventType.Update,
-        timestamp: timestamp.timestamp
-      })),
-      startWith<CacheEvent>({ type: CacheEventType.Initial, timestamp: undefined }),
-      merge<CacheEvent>(resetEvents),
-      shareReplay(CACHE_SIZE)
-    )
-    this.responses = this.requests.pipe(
+    this.responses$ = this.requests$.pipe(
       switchMap(_ => observableOperation$),
       shareReplay(CACHE_SIZE)
     )
-    this.responseReceived = this.responses.pipe(
-      map(_ => true),
-      startWith(false),
-      shareReplay(CACHE_SIZE)
+
+    const resetEvents$ = this.resetCacheRequests$.pipe(toCacheEvent(CacheEventType.Reset))
+    const requestEvents$ = this.requests$.pipe(toCacheEvent(CacheEventType.Request))
+    const responseEvents$ = this.responses$.pipe(toCacheEvent(CacheEventType.Response))
+    const cacheEvents$ = merge(resetEvents$, requestEvents$, responseEvents$).pipe(
+      startWith({ type: CacheEventType.Initial, timestamp: INITIAL_TIME })
     )
-    this.responseEvents = this.responses.pipe(
-      timestamp(),
-      map<Timestamp<T>, CacheEvent>(timestamp => ({
-        type: CacheEventType.Update,
-        timestamp: timestamp.timestamp
-      })),
-      startWith<CacheEvent>({ type: CacheEventType.Initial, timestamp: undefined }),
-      merge<CacheEvent>(resetEvents),
+
+    this.cacheState$ = cacheEvents$.pipe(
+      scan<CacheEvent, CacheState>(
+        (cacheState, cacheEvent) => {
+          const lastRequestTimestamp =
+            cacheEvent.type === CacheEventType.Request
+              ? cacheEvent.timestamp
+              : cacheState.lastRequestTimestamp
+          const lastResponseTimestamp =
+            cacheEvent.type === CacheEventType.Response
+              ? cacheEvent.timestamp
+              : cacheState.lastResponseTimestamp
+          return {
+            lastRequestTimestamp,
+            lastResponseTimestamp,
+            lastEvent: cacheEvent.type,
+            lastEventTimestamp: cacheEvent.timestamp
+          }
+        },
+        {
+          lastEvent: CacheEventType.Initial,
+          lastEventTimestamp: INITIAL_TIME,
+          lastRequestTimestamp: INITIAL_TIME,
+          lastResponseTimestamp: INITIAL_TIME
+        }
+      ),
       shareReplay(CACHE_SIZE)
     )
   }
 
   invalidateCache(): void {
-    this.resetCache.next()
+    this.resetCacheRequests$.next()
   }
 
-  get(expiry: number = 0, returnExpiredCached: boolean = false): Observable<T> {
-    return combineLatest(this.requestEvents, this.responseEvents, this.responseReceived).pipe(
+  get(ifNotOlderThan: number = 0, returnExpiredCached: boolean = false): Observable<T> {
+    return this.cacheState$.pipe(
       take(1),
       timestamp(),
-      flatMap(({ value: [lastRequestEvent, lastResponseEvent, responseReceived], timestamp }) => {
-        if (shouldMakeRequest(lastRequestEvent, lastResponseEvent, timestamp, expiry)) {
-          this.requests.next()
-          if (responseReceived && !returnExpiredCached) {
-            return this.responses.pipe(skip(1))
+      flatMap(({ value: cacheState, timestamp }) => {
+        if (shouldMakeRequest(cacheState, timestamp, ifNotOlderThan)) {
+          this.requests$.next()
+          if (cacheState.lastResponseTimestamp !== INITIAL_TIME && !returnExpiredCached) {
+            return this.responses$.pipe(skip(1))
           }
         }
-        return this.responses
+        return this.responses$
       })
     )
   }
